180e42c8a08894c7af56945df662ae0b255f3fa4
Doug Martin <dmartin@concord.org>
2019-08-06 06:50:57 -0400

    Express setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,29 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const apiV1 = express();
+const main = express();
+
+main.use('/api/v1', apiV1);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+export const webApiV1 = functions.https.onRequest(main);
+
+apiV1.get('/resources', (req, res) => {
+  res.send("TODO: find resource...");
+})
+
+apiV1.post('/resources', (req, res) => {
+  res.send("TODO: create resource...");
+})
+
+apiV1.patch('/resources/:id', (req, res) => {
+  res.send("TODO: update resource...");
+})
+
+apiV1.post('/resources/aws-keys', (req, res) => {
+  res.send("TODO: create aws keys from resource...");
+})
+
