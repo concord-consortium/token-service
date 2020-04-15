e8fc0c5767d5090eba6a97bfeb53632a92e3c877
pjanik <janikpiotrek@gmail.com>
2020-04-15 18:06:38 +0200

    Add "allowedAccessRuleTypes" to resource settings object, refactor settings interfaces
    [#172225423]

--- functions/src/firestore-types.ts
+++ functions/src/firestore-types.ts
@@ -1,7 +1,10 @@
-import { S3Resource, IotResource } from "./resource-types";
+import { S3Resource, IotResource, AccessRuleType, ResourceType } from "./resource-types";
 
 export type FireStoreResource = FireStoreS3Resource | FireStoreIotOrganizationResource;
 
+// S3Resource is client-facing interface. Some of the fields are marked as optional (e.g. accessRules) as user
+// might not have access to them. Firestore version represents data stored in Firestore, so getters are omitted
+// and all the fields are marked as present/required.
 export interface FireStoreS3Resource extends Required<Omit<S3Resource, "id" | "publicPath" | "publicUrl">> {
   type: "s3Folder"
 }
@@ -9,14 +12,23 @@ export interface FireStoreIotOrganizationResource extends Required<Omit<IotResou
   type: "iotOrganization"
 }
 
-export type FireStoreResourceSettings = FireStoreS3ResourceSettings;
+export type FireStoreResourceSettings = FireStoreS3ResourceSettings | FirestoreIotOrganizationSettings;
 
-export interface FireStoreS3ResourceSettings {
-  type: "s3Folder";
+export interface ResourceSettings {
+  type: ResourceType;
   tool: string;
+  allowedAccessRuleTypes: AccessRuleType[];
+}
+
+export interface FireStoreS3ResourceSettings extends ResourceSettings {
+  type: "s3Folder";
   bucket: string;
   folder: string;
   region: string;
+}
+
+export interface FirestoreIotOrganizationSettings extends ResourceSettings {
+  type: "iotOrganization";
 }
 
 export interface JWTClaims {
